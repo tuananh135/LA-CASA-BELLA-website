d256f7741a542887e0613efdf3c5a854218c8e5b
tuan anh <anhlt@thinhtientech.com>
2021-02-23 23:00:31 +0700

    Init project

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb://localhost:27017/phenikaa?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false' 
+
+module.exports = {
+    mongoURI: dbPassword
+};
